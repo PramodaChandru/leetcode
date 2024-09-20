91f8e1d3964c700ec7535de8e00e2c723ec15b5e
PramodCGowda <pramodcgwd50@gmail.com>
2024-09-19 22:15:19 -0400

    added new solution

--- src/Strings/UniqChar_387.java
+++ src/Strings/UniqChar_387.java
@@ -1,4 +1,30 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class UniqChar_387 {
+    public static int firstUniqChar(String s) {
+        int result = Integer.MAX_VALUE;
+        for(char c = 'a'; c <= 'z'; c++) {
+            int index = s.indexOf(c);
+            if(index != -1 && index == s.lastIndexOf(c)) {
+                result = Math.min(result, index);
+            }
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s = sc.nextLine();
+        System.out.println(firstUniqChar(s));
+
+        System.out.println("Test 1 result ====> " + test("leet", 0));
+        System.out.println("Test 2 result ====> " + test("leel", -1));
+    }
+
+    public static boolean test(String input, int output) {
+        return firstUniqChar(input) == output;
+    }
 }
