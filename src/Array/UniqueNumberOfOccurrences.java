fe2fb0bdd3fa17d70d6a07697a9a617544b151b1
PramodCGowda <pramodcgwd50@gmail.com>
2024-10-07 22:24:15 -0400

    new message

---
+++ src/Array/UniqueNumberOfOccurrences.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    private static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> countOccurrence = new LinkedHashMap<>();
+
+        for(int num : arr) {
+            countOccurrence.put(num, countOccurrence.getOrDefault(num, 0) + 1);
+        }
+
+        Set<Integer> resultSet = new HashSet<>(countOccurrence.values());
+
+        return countOccurrence.size() == resultSet.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3}));
+        System.out.println(uniqueOccurrences(new int[]{1, 2}));
+        System.out.println(uniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
+    }
+}
