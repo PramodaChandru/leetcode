7421c4b4fbee9dd6f1a7b08570f2011f5a47c583
PramodCGowda <pramodcgwd50@gmail.com>
2024-12-23 12:32:36 -0500

    45 days challenge - day 2

--- src/Stack/RemovingStarsFromaString.java
+++ src/Stack/RemovingStarsFromaString.java
@@ -1,23 +1,45 @@
 package Stack;
 
+/**
+ 2390. Removing Stars From a String
+ Given:
+ String s contains stars and lower case english letters.
+ For each operation remove the * and the letter to its left
+
+ Time Complexity: O(n) - each character is visited at most once
+ Space Complexity: O(1) - No additional data structures are used
+ */
+
 import java.util.Stack;
 
 public class RemovingStarsFromaString {
-    private static String removeStars(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length();i++) {
-            if(s.charAt(i) != '*') {
-                stack.push(s.charAt(i));
-            } else {
-                if(!stack.isEmpty()) {
-                    stack.pop();
-                }
-            }
-        }
+//    private static String removeStars(String s) {
+//        Stack<Character> stack = new Stack<>();
+//        for(int i = 0; i < s.length();i++) {
+//            if(s.charAt(i) != '*') {
+//                stack.push(s.charAt(i));
+//            } else {
+//                if(!stack.isEmpty()) {
+//                    stack.pop();
+//                }
+//            }
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        for(Character c : stack) {
+//            sb.append(c);
+//        }
+//        return sb.toString();
+//    }
 
+    private static String removeStars(String s) {
         StringBuilder sb = new StringBuilder();
-        for(Character c : stack) {
-            sb.append(c);
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != '*') {
+                sb.append(s.charAt(i));
+            } else {
+                sb.deleteCharAt(sb.length() - 1);
+            }
         }
         return sb.toString();
     }
