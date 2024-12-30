6bb016f1e0fa8785a8988cd614f66bcb088b724a
PramodCGowda <pramodcgwd50@gmail.com>
2024-12-30 00:02:42 -0500

    45 days challenge - day7

--- src/prfixSum/FindTheHighestAltitude.java
+++ src/prfixSum/FindTheHighestAltitude.java
@@ -18,10 +18,16 @@ package prfixSum;
 class FindTheHighestAltitude {
     public static void main(String[] args) {
         System.out.println(largestAltitude(new int[]{-5, 1, 5, 0, -7}));
+        System.out.println(largestAltitude(new int[]{-4,-3,-2,-1,4,3,2}));
     }
     private static int largestAltitude(int[] gain) {
         int maxValue = 0;
         int arr[] = new int[gain.length + 1];
+        arr[0] = 0;
+        for(int i = 1; i < arr.length; i++ ) {
+            arr[i] = arr[i - 1] + gain[i -1];
+            maxValue = Math.max(maxValue, arr[i]);
+        }
 //        int alt = 0;
 //        for(int n : gain) {
 //            alt += n;
